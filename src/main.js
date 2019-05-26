9b227aec9b27e917273cbbe285e4c0b560260f0c
Aashay <aashay.trivedi1@gmail.com>
2019-05-26 12:33:48 +0530

    first

--- src/main.js
+++ src/main.js
@@ -2,8 +2,18 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import Adsense from 'vue-google-adsense/dist/Adsense.min.js'
+import InArticleAdsense from 'vue-google-adsense/dist/InArticleAdsense.min.js'
+import InFeedAdsense from 'vue-google-adsense/dist/InFeedAdsense.min.js'
+
 
 Vue.config.productionTip = false
+
+Vue.use(require('vue-script2'))
+
+Vue.use(Adsense)
+Vue.use(InArticleAdsense)
+Vue.use(InFeedAdsense)
 
 new Vue({
   router,
