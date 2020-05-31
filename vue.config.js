b18a06a273e112bee12ab0055b0603caae2fe668
Aashay <aashay.trivedi1@gmail.com>
2020-05-31 19:19:08 +0530

    first

--- vue.config.js
+++ vue.config.js
@@ -11,7 +11,7 @@ module.exports = {
           // Absolute path to compiled SPA
           path.resolve(__dirname, 'dist'),
           // List of routes to prerender
-          [ '/', '/binary-to-hexadecimal', '/hexadecimal-to-decimal', '/wake-on-lan', '/lan-local-area-network', '/octal-to-binary-converter', '/decimal-to-binary', '/octal-to-decimal', '/decimal-to-hexadecimal', '/hexadecimal-to-octal', '/decimal-to-octal', '/hexadecimal-to-binary-converter', '/qr-code-reader', '/qr-code-generator', '/about-creator', '/blog', '/binary-to-decimal', '/binary-to-octal', '/write-for-us', '/ascii-to-binary', '/binary-converter', '/2-in-binary', '/write-6-in-binary'],
+          [ '/', '/binary-to-hexadecimal', '/hexadecimal-to-decimal', '/wake-on-lan', '/lan-local-area-network', '/octal-to-binary-converter', '/decimal-to-binary', '/octal-to-decimal', '/decimal-to-hexadecimal', '/hexadecimal-to-octal', '/decimal-to-octal', '/hexadecimal-to-binary-converter', '/qr-code-reader', '/qr-code-generator', '/about-creator', '/blog', '/binary-to-decimal', '/binary-to-octal', '/write-for-us', '/ascii-to-binary', '/binary-converter', '/2-in-binary', '/write-6-in-binary', 'how-to-write-10-in-binary'],
         )
       ]
     }
